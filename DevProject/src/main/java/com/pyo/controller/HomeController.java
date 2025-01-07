ddbeb205cba413905906039a634cfd7776418806
jangpyo <wkdvy122851@gmail.com>
2025-01-07 16:24:54 +0900

    [CREATE] ajax 비동기 처리방식 테스트

--- DevProject/src/main/java/com/pyo/controller/HomeController.java
+++ DevProject/src/main/java/com/pyo/controller/HomeController.java
@@ -8,11 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @Controller
 public class HomeController {
 	private static final Logger Logger = LoggerFactory.getLogger(HomeController.class);
@@ -27,5 +30,10 @@ public class HomeController {
 		Logger.info("logger2"+model);
 		System.out.println("sysout2" + model);
 		return "home";
+	}
+
+	@GetMapping(value="/ajaxhome")
+	public String ajaxHome() {
+		return "ajaxhome";
 	}
 }
