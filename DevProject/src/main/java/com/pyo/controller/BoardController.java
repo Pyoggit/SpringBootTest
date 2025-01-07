bd6549f1913edce63cea785d0abed3a92f09b450
jangpyo <wkdvy122851@gmail.com>
2025-01-07 18:04:06 +0900

    [UPDATE] params 파라미터 GET 방식 변경

--- DevProject/src/main/java/com/pyo/controller/BoardController.java
+++ DevProject/src/main/java/com/pyo/controller/BoardController.java
@@ -5,52 +5,57 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
-@Log
+@Slf4j
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-// @RequestMapping의 value 속성에 요청 경로를 설정한다.
-// register 경로에 GET 방식 설정
-	@GetMapping(value = "/register")
+	@GetMapping(value = "/get", params = "register")
 	public String registerForm() {
-		log.info("GET 방식 등록 폼");
-		return "success";
+		log.info("params 파라미터 GET 방식 등록 폼");
+		return "board/register";
 	}
 
-// register 경로에 POST 방식 설정
-	@PostMapping(value = "/register")
+	@PostMapping(value = "/post", params = "register")
 	public String register() {
-		log.info("POST 방식 등록");
-		return "success";
+		log.info("params 파라미터 POST 방식 등록");
+		return "board/list";
 	}
 
-//modify 경로에 GET 방식
-	@GetMapping(value = "/modify")
+	@GetMapping(value = "/get", params = "modify")
 	public String modifyForm() {
-		log.info("GET 방식 수정 폼");
-		return "success";
+		log.info("params 파라미터 GET 방식 수정 폼");
+		return "board/modify";
 	}
 
-//modify 경로에 POST 방식
-	@PostMapping(value = "/modify")
+	@PostMapping(value = "/post", params = "modify")
 	public String modify() {
-		log.info("POST 방식 수정");
-		return "success";
+		log.info("params 파라미터 POST 방식 수정");
+		return "board/list";
 	}
 
-//remove 경로에 POST 방식
-	@PostMapping(value = "/remove")
+	@GetMapping(value = "/get", params = "remove")
+	public String removeForm() {
+		log.info("params 파라미터 GET 방식 삭제 폼");
+		return "board/remove";
+	}
+
+	@PostMapping(value = "/post", params = "remove")
 	public String remove() {
-		log.info("POST 방식 삭제");
-		return "success";
+		log.info("params 파라미터 POST 방식 삭제");
+		return "board/list";
 	}
 
-//list 경로에 GET 방식
-	@GetMapping(value = "/list")
+	@GetMapping(value = "/get", params = "list")
 	public String list() {
-		log.info("GET 방식 목록");
-		return "success";
+		log.info("params 파라미터 GET 방식 목록");
+		return "board/list";
+	}
+
+	@GetMapping(value = "/get", params = "read")
+	public String read() {
+		log.info("params 파라미터 GET 방식 읽기");
+		return "board/read";
 	}
 }
