12aa4084b1905f6499e9c65966f33fad81222112
jangpyo <wkdvy122851@gmail.com>
2025-01-09 11:08:58 +0900

    [UPDATE] 파일업로드 기능 작성

--- DevProject/src/main/java/com/pyo/controller/MemberController.java
+++ DevProject/src/main/java/com/pyo/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.pyo.controller;
 
+import java.io.File;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pyo.domain.Address;
 import com.pyo.domain.Member;
@@ -21,13 +25,23 @@ public class MemberController {
 		log.info("member.getPassword() = " + member.getPassword());
 		log.info("member.getCoin() = " + member.getCoin());
 		log.info("Date dateOfBirth = " + member.getDateOfBirth());
-		if(member.getCar() != null) {
-			for(String data : member.getCar()) {
-				log.info("membercar = " + data);		
-			}
+		log.info("member" + member);
+		log.info("address" + address);
+		return "home";
+	}
+
+	@RequestMapping(value = "/registerFileUp01", method = RequestMethod.POST)
+	public String registerFileUp01(MultipartFile picture) throws Exception {
+		log.info("registerFileUp01");
+		log.info("originalName: " + picture.getOriginalFilename());
+		log.info("size: " + picture.getSize());
+		log.info("contentType: " + picture.getContentType());
+		
+		if(!picture.isEmpty()){
+		    String fileName = picture.getOriginalFilename();
+		    picture.transferTo(new File("C:/SpringBootProject/upload_files/"+fileName));
 		}
-		log.info("member.toString"+ member);
-		log.info("address.toString"+ address);
+		
 		return "home";
 	}
 
