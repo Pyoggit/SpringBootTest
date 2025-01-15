68304d9074691a5bcc6d57ac19cb4080ac02c7b8
jangpyo <wkdvy122851@gmail.com>
2025-01-15 21:36:08 +0900

    [CREATE] Board 테이블 모델링

---
+++ SpringBootAOP/src/main/java/com/zeus/domain/Board.java
@@ -0,0 +1,18 @@
+package com.zeus.domain;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Board {
+
+	private int boardNo; 
+	@NotBlank
+	private String title; 
+	private String content; 
+	private String writer; 
+	private Date regDate;
+
+}
